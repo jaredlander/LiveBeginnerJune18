e5c86ca9c485103a64881e36d6ce13732c035df0
Jared Lander <info@jaredlander.com>
2018-06-19 11:01:17 -0400

    Showed formula interface

--- code/LinearModels.r
+++ code/LinearModels.r
@@ -13,3 +13,21 @@ height1
 33.8866 + 0.5141*60
 33.8866 + 0.5141*50
 33.8866 + 0.5141*72
+
+boros <- tibble::tribble(
+    ~ Boro, ~ Pop, ~ Size, ~ Random,
+    'Manhattan', 1600000, 23, 7,
+    'Brooklyn', 2600000, 78, 24,
+    'Queens', 2330000, 104, pi,
+    'Bronx', 1455000, 42, 21,
+    'Staten Island', 475000, 60, 3
+)
+boros
+
+library(useful)
+
+build.x( ~ Pop, data=boros)
+build.x( ~ Pop + Size, data=boros)
+build.x( ~ Pop + Size + Random, data=boros)
+
+build.x( ~ Pop + Boro, data=boros)
